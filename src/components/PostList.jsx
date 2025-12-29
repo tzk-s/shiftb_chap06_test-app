4b0fa56fcda6fb82be40e82f92d4d28fc2f73afa
tzk-s <121493194+tzk-s@users.noreply.github.com>
2025-12-29 17:06:51 +0900

    不要thumbnailUrl削除、HTMLタグ修正

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -4,8 +4,8 @@ export default function PostsList({ posts }) {
   return (
     <ul className="max-w-[800px] w-10/12 mx-auto mt-10 space-y-6">
       {posts.map(post => (
-        <li key={post.id} className="text-left text-black border border-gray-200 block p-5 hover:shadow-md transition duration-300 cursor-pointer">
-          <a href={post.thumbnailUrl}>
+        <li key={post.id} className="text-left text-black border border-gray-200 block p-5">
+          <div>
             <div className="flex justify-between items-start">
               <div>
                 <time dateTime={post.createdAt} className="text-[0.8rem] text-[#888]">
@@ -23,8 +23,8 @@ export default function PostsList({ posts }) {
             <h2 className="text-[1.5rem] mt-2 text-gray-800">
               {`APIで取得した${post.title}`}
             </h2>
-            <p className="text-[1rem] mt-2 text-gray-600 line-clamp-3" dangerouslySetInnerHTML={formatContent(post.content)}></p>
-          </a>
+            <div className="text-[1rem] mt-2 text-gray-600 line-clamp-3" dangerouslySetInnerHTML={formatContent(post.content)}></div>
+          </div>
         </li>
       ))}
     </ul>
